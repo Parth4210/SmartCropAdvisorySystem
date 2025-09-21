64ce8b294720c606925836f8bae4cf484e6190f0
DevAadityaNath <flameworksofficial007@gmail.com>
2025-09-21 07:04:51 +0530

    Added Warnings On the Main Page.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +58,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // In your dependencies { ... } block
+
+// This is for the @Preview annotation itself
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+// This is for the preview functionality in the IDE.
+// It's a `debugImplementation` because it's only needed for debugging and previewing.
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
